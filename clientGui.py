186ea472d6bc21f64e099d3f047eb6008c8b1d8a
Melih Telli <melih_telli_3232@hotmail.com>
2018-03-19 23:39:32 +0300

    konusma düzeltilmis

--- clientGui.py
+++ clientGui.py
@@ -1,34 +1,30 @@
+#!/usr/bin/env python3
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 import time
 import socket
 import json
 import sys
 import threading
-
-
-"""
-class uiTxtEdtGunclThread(QtCore.QThread):
-    def __init__(self,uiwin):
-        super().__init__(parent=QtCore.QThread())
-        self.uiwin =uiwin
-
-    def run(self):
-        while True:
-            self.uiwin.textEdit.setText(self.uiwin.mesajlar)
-            time.sleep(0.1)
-"""
-
-
+import datetime
+import random
+from Crypto.Cipher import AES
+import os
 
 
 class Ui_MainWindow(object):
-    degDeger = QtCore.pyqtSignal(str)
-
     def __init__(self):
+        self.isim = "Melih"
+        self.id_kull = datetime.datetime.now().microsecond + random.randint(100000, 999999)
         self.istemci = Istemci()
         self.mesajlar = ""
+        self.msjsayisi = 0
+        self.key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+        self.cipher = AES.new(self.key)
+        self.kullanicilar = []
+        self.glnmsjsayisi = []
+        self.kullanicilar.append(self.id_kull)
+        self.glnmsjsayisi.append(self.msjsayisi)
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -91,77 +87,71 @@ class Ui_MainWindow(object):
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def gelenMesajKntTimer(self):
+        if self.istemci.mesajAl() == "boss":
+            pass
+        else:
+            print(self.kullanicilar)
+            print(self.glnmsjsayisi)
+            gelenmsj = json.loads(self.decrypt(self.istemci.mesajAl()))
 
-    """
-    def txtEdtgun_Olay(self,dgr):
-        self.textEdit.
-    """
+            if gelenmsj[2] in self.kullanicilar:
+                if self.glnmsjsayisi[self.kullanicilar.index(gelenmsj[2])] == gelenmsj[3]:
 
-    @QtCore.pyqtSlot(str)
-    def txtEdtGun(self,degr):
-        self.textEdit.setText(degr)
-
-    def gelenMesajKnt(self):
-
-
-        while True:
-            glnmsj = self.istemci.mesajAl()
-            if glnmsj == "boss":
-                pass
-            else:
-                if self.mesajlar == (self.mesajlar + glnmsj+"\n"):
                     pass
-                else:
-                    self.mesajlar += glnmsj+"\n"
+                elif gelenmsj[3] > self.glnmsjsayisi[self.kullanicilar.index(gelenmsj[2])]:
+
+                    self.glnmsjsayisi[self.kullanicilar.index(gelenmsj[2])] = gelenmsj[3]
+                    self.mesajlar += gelenmsj[0] + " : " + gelenmsj[1] + "\n"
+                    self.textEdit.setText(self.mesajlar)
 
 
-            time.sleep(0.1)
-
+            else:  # gelenmsj[2] in self.kullanicilar:
+                self.kullanicilar.append(gelenmsj[2])
+                self.glnmsjsayisi.append(gelenmsj[3])
+                self.mesajlar += gelenmsj[0] + " : " + gelenmsj[1] + "\n"
+                self.textEdit.setText(self.mesajlar)
 
     def gonderClick(self):
-
 
         if self.lineEdit.text() == "":
             pass
         else:
-            self.istemci.mesajGonder(self.lineEdit.text())
-            self.mesajlar += self.lineEdit.text()+"\n"
-            self.textEdit.setText(self.mesajlar)
+            self.msjsayisi += 1
+            gondmess = [self.isim, self.lineEdit.text(), self.id_kull, self.msjsayisi]
+            gondmess = self.encrypt(json.dumps(gondmess))
+
+            self.istemci.mesajGonder(gondmess)
 
             self.lineEdit.clear()
 
-        print("mesajlar"+ self.mesajlar)
-
-
-
     def baglantiClick(self):
-        host = self.uiB.lineEdit.text()
-        port = self.uiB.lineEdit_2.text()
+        if self.istemci.durum():
+            pass
+        else:
+            host = self.uiB.lineEdit.text()
+            port = self.uiB.lineEdit_2.text()
+            self.isim = self.uiB.lineEdit_3.text()
 
-        print("host : " + host)
-        print("port : " + port)
+            print("host : " + host)
+            print("port : " + port)
 
-        self.istemci.baslat(host, int(port))
+            self.istemci.baslat(host, int(port))
 
-        glnmsjknt = threading.Thread(target=self.gelenMesajKnt,)
-        glnmsjknt.start()
+            self.timer = QtCore.QTimer()
+            self.timer.timeout.connect(self.gelenMesajKntTimer)
+            self.timer.start(10)
 
+            self.FormDailog.close()
 
-
-
-
-
-
-
-
-
-    def menuClick(self,action):
-        if action.text()=="&Bağlan":
-            Form = QtWidgets.QDialog()
+    def menuClick(self, action):
+        if action.text() == "&Bağlan":
+            self.FormDailog = QtWidgets.QDialog()
             self.uiB = Baglan_Ui()
-            self.uiB.setupUi(Form)
+            self.uiB.setupUi(self.FormDailog)
             self.uiB.pushButton.clicked.connect(self.baglantiClick)
-            Form.exec_()
+            self.FormDailog.exec_()
+
 
         elif action.text() == "&Çıkış":
             Dilog = QtWidgets.QDialog()
@@ -170,13 +160,22 @@ class Ui_MainWindow(object):
             uyari.label.setText("Bağlantıyı kesmek istediğinize emin misiniz ??")
 
             if Dilog.exec_():
+                self.timer.stop()
                 self.istemci.durdur()
             else:
                 pass
 
+    # Şifreleme Fonksiyonları
+    def pad(self, s):
+        return s + ((16 - len(s) % 16) * '{')
 
+    def encrypt(self, plaintext):
+        return self.cipher.encrypt(self.pad(plaintext))
 
-
+    def decrypt(self, ciphertext):
+        dec = str(self.cipher.decrypt(ciphertext).decode('utf-8'))
+        l = dec.count('{')
+        return dec[:len(dec) - l]
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -187,57 +186,69 @@ class Ui_MainWindow(object):
         self.action_k.setText(_translate("MainWindow", "&Çıkış"))
         self.actionTema.setText(_translate("MainWindow", "&Tema"))
 
+
 class Istemci():
 
     def __init__(self):
         global mesajj
-        mesajj= "boss"
+        mesajj = "boss"
+        global onoffknt
+        onoffknt = False
 
-
-
-    def baslat(self,h,p):
-
+    def baslat(self, h, p):
+        global onoffknt
         self.host = h
         self.port = p
         self.tampon = 1024
 
-        self.sock = socket.socket()
-        self.sock.connect((h, p))
+        try:
+            self.sock = socket.socket()
+            self.sock.connect((h, p))
+            glnmes = threading.Thread(target=self.gelenMessaj, )
+            glnmes.start()
 
-
-        glnmes =  threading.Thread(target=self.gelenMessaj,)
-        glnmes.start()
-
+            onoffknt = True
+        except:
+            Dilog = QtWidgets.QDialog()
+            uyari = UyariDialog()
+            uyari.setupUi(Dilog)
+            uyari.label.setText("Bağlantı Kurulamadı")
+            Dilog.exec()
+            onoffknt = False
 
     def durdur(self):
         self.sock.close()
-
+        global onoffknt
+        onoffknt = False
 
     def gelenMessaj(self):
         global mesajj
-        while True:
-            data =  self.sock.recv(self.tampon)
+        global onoffknt
+        try:
+            while True:
+                data = self.sock.recv(self.tampon)
 
-            if data =="":
-                pass
-            elif data == bytes():
-                mesajj="Server Baglantı koptu"
-                break
-            else:
-                mesajj=bytes.decode(data,"utf-8") + "  TT"
+                if data == "":
+                    pass
+                elif data == bytes():
+                    mesajj = "Server Baglantı koptu"
+                    break
+                else:
+                    mesajj = data
 
+                time.sleep(0.02)
+        except Exception as problem:
+            print(problem)
+            onoffknt = False
 
-            time.sleep(0.02)
-
-
-    def mesajGonder(self,messaj):
-        mess = bytes(messaj,"utf-8")
-        self.sock.send(mess)
-        print("mesaj gonderildi")
+    def mesajGonder(self, messaj):
+        self.sock.send(messaj)
 
     def mesajAl(self):
         return mesajj
 
+    def durum(self):
+        return onoffknt
 
 
 class Baglan_Ui(object):
@@ -272,24 +283,25 @@ class Baglan_Ui(object):
         self.lineEdit.setText("127.0.0.1")
         self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
 
+        self.lineEdit_3 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_3.setText("Melih")
+        self.gridLayout.addWidget(self.lineEdit_3, 3, 1, 1, 1)
+
         self.label_3 = QtWidgets.QLabel(Form)
         self.label_3.setObjectName("label_3")
         self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
-        self.label_4 = QtWidgets.QLabel(Form)
-        self.label_4.setText("")
-        self.label_4.setObjectName("label_4")
-        self.gridLayout.addWidget(self.label_4, 3, 1, 1, 1)
+
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout.addLayout(self.verticalLayout, 1, 1, 1, 1)
         self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.gridLayout.addLayout(self.verticalLayout_2, 1, 0, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
-
-
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
@@ -297,11 +309,7 @@ class Baglan_Ui(object):
         self.label_2.setText(_translate("Form", "İP :"))
         self.label.setText(_translate("Form", "PORT :"))
         self.pushButton.setText(_translate("Form", "Bağlan"))
-        self.label_3.setText(_translate("Form", "Durum :"))
-
-    def uiclose(self):
-        QtWidgets.QDialog.close(self)
-
+        self.label_3.setText(_translate("Form", "İsim :"))
 
 
 class UyariDialog(object):
@@ -315,7 +323,7 @@ class UyariDialog(object):
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
 
@@ -330,13 +338,20 @@ class UyariDialog(object):
         self.label.setText(_translate("Dialog", "TextLabel"))
 
 
-
-
 if __name__ == "__main__":
+    def myExitHandler():
+        ui.istemci.durdur()
+        ui.timer.stop()
+        os._exit(0)
+
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    app.aboutToQuit.connect(myExitHandler)
+
     sys.exit(app.exec_())
+
 
--- konusmaServer.py
+++ konusmaServer.py
@@ -1,8 +1,6 @@
+#!/usr/bin/env python3
 import threading
 import socket
-import os
-import sys
-import time
 
 class Server():
     def __init__(self,h,p,u):
@@ -19,23 +17,15 @@ class Server():
 
         bagkbl = threading.Thread(target=self.bagKabul,)
         bagkbl.start()
-        print("Server Baslatıldı")
 
 
     def bagKabul(self):
         for i in range(self.user):
             self.baglnt,self.adrr = self.sock.accept()
             self.baglantilar.append(self.baglnt)
-            #self.baglnt.getpeername()
-
-            self.baglnt.send(bytes("server Baglandin","utf-8"))
-
             for i in range(len(self.baglantilar)):
-                #self.messajGonder(i)
-
                 msjgndthrd = threading.Thread(target=self.messajGonder,args=(i,))
                 msjgndthrd.start()
-
 
             bagkbl = threading.Thread(target=self.bagKabul, )
             bagkbl.start()
@@ -43,8 +33,6 @@ class Server():
 
 
     def messajGonder(self,kulS):
-        print("mesaj gonder fonk")
-
         try:
             while True:
                 data = self.baglantilar[kulS].recv(self.tampon)
@@ -54,7 +42,6 @@ class Server():
                 for i in range(len(self.baglantilar)):
                     print("Messaj gonder for içi : "+ str(i))
                     self.baglantilar[i].send(data)
-
 
                 if not data :
                     print(str(self.baglantilar[kulS].getpeername())+" Hata")
@@ -70,10 +57,11 @@ class Server():
                     break
 
         except Exception as problem :
+
             print(str(problem ))
 
 
 
 
 if __name__ =="__main__":
-    server = Server("127.0.0.1",4446,5)
+    server = Server("192.168.1.34",31312,5)
